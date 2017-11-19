eeb339f62110fada2c61498e86f162ccd629bc04
Ishmukhametov Ruslan <ishmukruslan@gmail.com>
2017-11-19 22:34:10 +0300

    task 3

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
  function addTask(s) {
-    var task = $('<li></li>');
-    $('li', task).append('<span></span>');
+    var task = $('<li><span></span> <button class = "delete_task">Удалить</button></li>');
     $('span', task).text(s);
-    $('li', task).append('<button class = "delete_task">Удалить</button>');
     $('.delete_task', task).click(function(){$(this).parent().remove()});
     $('#root ul').append(task);
 }
@@ -11,6 +9,5 @@ $(function(){
     $('#root').append('<ul></ul>');
     addTask("Сделать задание #3 по web-программированию");
     $('#root').append('<input id="add_task_input"> <button id="add_task">Добавить</button>');
-    $('#add_task').click(function(){addElement($('#add_task_input').attr('id'))});
-    
+    $('#add_task').click(function(){addTask($('#add_task_input').val())});
 })
